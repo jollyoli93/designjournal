f9d5a0af57d841f5ce07f429cdd31d3d2a13e622
jollyoli93 <jackoliver1993@yahoo.co.uk>
2024-07-03 22:05:06 +0100

    horizontal rule added for first image

--- src/app/components/Contents/Contents.js
+++ src/app/components/Contents/Contents.js
@@ -7,26 +7,27 @@ import Image from 'next/image';
 
 export default function Contents () {
     function ListContent () {
-        return content.map(item => (
-        <div className="pt-10" key={item.key}>{
-            <div className="px-5 gap-10 flex flex-col md:flex-row lg:flex-row items-center">
-                <Image 
-                  src={item.url} 
-                  width={350} 
-                  height={50}
-                  style={{
-                    objectFit:"cover"
-                  }} 
-                  sizes="100vw"
-                  alt=""/> 
-                <div className="px-5">
-                    <h2 className="py-2 font-black md:text-xl lg:text-2xl">{item.heading}</h2>
-                    <p>{item.body}</p>
+        return content.map((item, index) => (
+            <div className="pt-10" key={item.key}>{
+                <div className="px-10 gap-10 flex flex-col md:flex-row lg:flex-row items-center">
+                    <Image 
+                    src={item.url} 
+                    width={350} 
+                    height={50}
+                    style={{
+                        objectFit:"cover"
+                        }}
+                    sizes="100vw"
+                    alt=""/> 
+                    <div className="px-5">
+                        <h2 className="py-2 font-black md:text-xl lg:text-2xl">{item.heading}</h2>
+                        <p>{item.body}</p>
+                    </div>
                 </div>
+                }
+                {index < 1 && <hr className="border-black my-2 mx-10"/>}  
             </div>
-            }
-        </div>
-    ))
+        ))
     }
     return (
         <div className="flex-1 border-2 border-black">
